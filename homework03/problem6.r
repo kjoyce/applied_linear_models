ef7530144ef6dbf0fd4d8ff1c702705bfedc44dc
Kevin Joyce <kevin.t.joyce@gmail.com>
2013-10-11 08:47:40 -0600

    Problem 6 finished

--- homework03/problem6.r
+++ homework03/problem6.r
@@ -2,8 +2,26 @@ shipment = read.csv('shipment.txt')
 attach(shipment)
 mdl = lm(time~drums+weight)
 X = model.matrix(mdl)
-t(X)%*%X 
+y = time
 
-X <- model.matrix(mdl)
-solve(t(X)%*%X)*summary(mdl)$sigma^2 
+# a
+(xtx = t(X)%*%X)
+(xty = t(X)%*%y)
+(yty = t(y)%*%y)
+(xtxi = solve(xtx))
+(betahat = solve(xtx,xty))
 
+# b
+(betavar = solve(t(X)%*%X)*summary(mdl)$sigma^2
+
+# c
+(t = (betahat[3] - 50)/sqrt(betavar[3,3]))
+
+# d
+(se_d = sqrt(betavar[2,2] + 4*betavar[3,3] + 4*betavar[2,3]))
+(t_d = (betahat[2] + 2*betahat[3])/se_d)
+
+# e
+round((est_d = betahat[2] + 2*betahat[3]),3)
+round((me_d = qt(.95,17)*se_d),3)
+cat("(",sprintf("%.3f",est_d + c(-1,1)*me_d),")\n")
